5276c9da052af13e26a983c60c798b81008d9298
Zachariah Crowell <zachariah.crowell@techtonic.com>
2019-02-26 12:39:36 -0700

    Stuff

---
+++ react-todo-list-app/src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+  <React.Fragment>
+
+      <h1>About</h1>
+      <p>Todo List App v1.0.0</p>
+
+  </React.Fragment>
+  )
+}
+ export default About;
